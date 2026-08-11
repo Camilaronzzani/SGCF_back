5373a2faf3b5664b1b869c4b690aa1fa5daf794f
Camila Luiza Ronzzani da Silva <camilasiva2863@gmail.com>
2026-08-11 19:31:45 -0300

    iniciado

---
+++ src/main/java/SGCF_back/Camilaronzzani/com/github/sgcf_back/SgcfBackApplication.java
@@ -0,0 +1,13 @@
+package SGCF_back.Camilaronzzani.com.github.sgcf_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcfBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcfBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SGCF_back/Camilaronzzani/com/github/sgcf_back/SgcfBackApplicationTests.java
@@ -0,0 +1,13 @@
+package SGCF_back.Camilaronzzani.com.github.sgcf_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcfBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
