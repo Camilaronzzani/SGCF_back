6a8191ce3b8326e82f18d6de2fc6f000a27329ec
Thurlex <alex.dias.gold@gmail.com>
2026-09-19 18:27:28 -0300

    Configure entity IDs as bigint identity columns

---
+++ src/main/resources/DB/Migration/V6__widen_entity_ids.sql
@@ -0,0 +1,8 @@
+alter table "customer" alter column "id" type bigint;
+alter table "employee" alter column "id" type bigint;
+alter table "tour" alter column "id" type bigint;
+alter table "user" alter column "id" type bigint;
+alter table "password_reset" alter column "id" type bigint;
+alter table "payment" alter column "id" type bigint;
+alter table "quota" alter column "id" type bigint;
+alter table "reservation" alter column "id" type bigint;
